86527950113d9ba808c135e9a266943df0143897
senthil kumar <senthilkumar.ramanathan@cognizant.com>
2019-02-16 13:52:27 +0530

    Repository pattern implemented in DAL

--- Outreach Reporting System/Outreach.Reporting.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Outreach.Reporting.Business
-{
-    public class Class1
-    {
-    }
-}
---
+++ Outreach Reporting System/Outreach.Reporting.Data/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Outreach.Reporting.Data.Interfaces
+{
+    class IRepository
+    {
+    }
+}
