ac3753bc51bbbe4f92065311a8592a2d68a485d8
senthil kumar <senthilkmaar@gmail.com>
2019-03-30 14:42:52 +0530

    repository test

--- RepositoryTest/Repositories/AssociateRepositoryTest.cs
+++ RepositoryTest/Repositories/AssociateRepositoryTest.cs
@@ -6,24 +6,35 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Moq;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Outreach_Reporting_System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Outreach.Reporting.Data.Entities;
 
 namespace RepositoryTest.Repositories
 {
     public class AssociateRepositoryTest
     {
         private readonly AssociateRepository _associateRepository;
-
-        private static readonly IConfigurationRoot Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
-
+        private readonly Mock<ReportDBContext> _dbContextMock;
         public AssociateRepositoryTest()
         {
-            var appSettingAccessorMock = new Mock<IOptions<AppSettings>>();
-            var appSettingAccessor = new AppSettings
-            {
-                DatabaseConnectionString = Config["connectionstring"]
-            };
-            appSettingAccessorMock.Setup(ap => ap.Value).Returns(appSettingAccessor);
-            //_associateRepository = new AssociateRepository(appSettingAccessorMock.Object);
+            _dbContextMock = new Mock<ReportDBContext>();
+            //var client = new TestClientProvider().Client;
+            //var response = await client.GetAsync("api/associate");
+            //response.EnsureSuccessStatusCode();
+          
+            //_associateRepository = new AssociateRepository(_dbContextMock.Object);
+        }
+
+        [Fact]
+        public void GetAll()
+        {
+            //var results = _associateRepository.GetAll();
+            //Assert.Empty(results);
         }
     }
 }
---
+++ RepositoryTest/Repositories/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Outreach_Reporting_System;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RepositoryTest.Repositories
+{
+    public class TestClientProvider
+    {
+        public HttpClient Client { get; private set; }
+
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = server.CreateClient();
+        }
+        }
+}
